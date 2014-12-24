d10cb52b39cf97d31c37ce1e5e056c1b7d3ac7f3
Shashank Shekhar <shashankjet@gmail.com>
2014-12-24 19:23:07 +0530

    events added

--- franklyapi/controllers.py
+++ franklyapi/controllers.py
@@ -556,21 +556,18 @@ def user_follow(cur_user_id, user_id):
     if cur_user_id == user_id:
         raise CustomExceptions.BadRequestException("Cannot follow yourself")
 
-    # add event
-    event = Event(id=get_item_id,user=cur_user_id,action='follow',foreign_data=user_id,event_time=datetime.datetime.now())
-
     db.session.execute(text("""INSERT INTO user_follows (user, followed, unfollowed) 
                                 VALUES(:cur_user_id, :user_id, false) 
                                 ON DUPLICATE KEY 
                                 UPDATE unfollowed = false"""),
                         params={'cur_user_id':cur_user_id, 'user_id':user_id}
                     )
+    
+    event = Event(user=cur_user_id, action='follow', foreign_data=user_id)
+    
     db.session.add(event)
     db.session.commit()
-    '''
-    notify_args = {'user1_id': cur_user_id,'user2_id': user_id}
-    cel_tasks.notify_mongo.delay('follow', **notify_args)
-    '''
+
     return {'user_id': user_id}
 
 
@@ -741,11 +738,10 @@ def question_ask(cur_user_id, question_to, body, lat, lon, is_anonymous):
 
     public = True if user_status['user_type']==2 else False #if user is celeb
     from database import get_item_id
-    # add event
     question = Question(id=get_item_id(), question_author=cur_user_id, question_to=question_to, 
                 body=body.capitalize(), is_anonymous=is_anonymous, public=public, lat=lat, lon=lon)
-    print question
-    event = Event(id=get_item_id,user=cur_user_id,action='question',foreign_data=question.id,event_time=datetime.datetime.now())
+    
+    event = Event(user=cur_user_id, action='question', foreign_data=question.id)
     db.session.add(question)
     db.session.add(event)
     db.session.commit()
@@ -792,14 +788,13 @@ def question_upvote(cur_user_id, question_id):
                             Question.question_to!=cur_user_id,
                             Question.deleted==False
                             ).count():
-        # add event
-        event = Event(id=get_item_id,user=cur_user_id,action='upvote',foreign_data=question_id,event_time=datetime.datetime.now())
         db.session.execute(text("""INSERT INTO question_upvotes (user, question, downvoted) 
                                     VALUES(:cur_user_id, :question_id, false) 
                                     ON DUPLICATE KEY 
                                     UPDATE downvoted = false"""),
                             params={'cur_user_id':cur_user_id, 'question_id':question_id}
                         )
+        event = Event(user=cur_user_id, action='upvote', foreign_data=question_id)
         db.session.add(event)
         db.session.commit()
     else:
@@ -838,14 +833,14 @@ def post_like(cur_user_id, post_id):
     answer_author, question_author= row
 
     if not (has_blocked(cur_user_id, answer_author) or has_blocked(cur_user_id, answer_author)):
-        # add event
-        event = Event(id=get_item_id,user=cur_user_id,action='like',foreign_data=post_id,event_time=datetime.datetime.now())
         db.session.execute(text("""INSERT INTO post_likes (user, post, unliked) 
                                     VALUES(:cur_user_id, :post_id, false) 
                                     ON DUPLICATE KEY 
                                     UPDATE unliked = false"""),
                             params={'cur_user_id':cur_user_id, 'post_id':post_id}
                             )
+
+        event = Event(user=cur_user_id, action='like', foreign_data=post_id)
         db.session.add(event)
         db.session.commit()
         #send notification
@@ -895,9 +890,9 @@ def comment_add(cur_user_id, post_id, body, lat, lon):
 
     if not (has_blocked(cur_user_id, answer_author) or has_blocked(cur_user_id, answer_author)):
         from database import get_item_id
-        # add event
         comment = Comment(id=get_item_id(), on_post=post_id, body=body, comment_author=cur_user_id, lat=lat, lon=lon)
-        event = Event(id=get_item_id,user=cur_user_id,action='comment',foreign_data=comment.id,event_time=datetime.datetime.now())
+        
+        event = Event(user=cur_user_id, action='comment', foreign_data=comment.id)
         db.session.add(comment)
         db.session.add(event)
         db.session.commit()
@@ -1254,7 +1249,7 @@ def add_video_post(cur_user_id, question_id, video, answer_type,
         video_url, thumbnail_url = media_uploader.upload_user_video(user_id=cur_user_id, video_file=video, video_type='answer')
         curruser = User.query.filter(User.id == cur_user_id).one()
         add_video_to_db(video_url, thumbnail_url)
-        # add event
+        
         post = Post(id=get_item_id(),
                     question=question_id,
                     question_author=question.question_author, 
@@ -1265,7 +1260,8 @@ def add_video_post(cur_user_id, question_id, video, answer_type,
                     client_id=client_id,
                     lat=lat,
                     lon=lon)
-        event = Event(id=get_item_id,user=cur_user_id,action='answer',foreign_data=post.id,event_time=datetime.datetime.now())
+        event = Event(user=cur_user_id, action='answer', foreign_data=post.id)
+        
         db.session.add(post)
         db.session.add(event)
         Question.query.filter(Question.id==question_id).update({'is_answered':True})
--- franklyapi/models/event.py
+++ franklyapi/models/event.py
@@ -2,25 +2,26 @@ import datetime
 from sqlalchemy import Column, Integer, String, Boolean, Float, DateTime, Enum, ForeignKey, CHAR, UniqueConstraint
 from database import Base
 from database import get_item_id
+from notification import consider_for_notification
 
 class Event(Base):
-    __tablename__             = 'events'
-    id                        = Column(CHAR(32), primary_key=True, default=get_item_id)
-    user                      = Column(CHAR(32), ForeignKey('users.id'), nullable=False)
-    action                    = Column(Enum('follow','like','upvote','answer','question','comment'), nullable=False)
-    foreign_data              = Column(CHAR(32), nullable=False)
-    event_time                = Column(DateTime(), default=datetime.datetime.now)
-    for_notification          = Column(Boolean(), default=True)
-    notification_sent         = Column(Boolean(), default=False)
+    __tablename__     = 'events'
+    id                = Column(CHAR(32), primary_key=True, default=get_item_id)
+    user              = Column(CHAR(32), ForeignKey('users.id'), nullable=False)
+    action            = Column(Enum('follow','like','upvote','answer','question','comment'), nullable=False)
+    foreign_data      = Column(CHAR(32), nullable=False)
+    event_time        = Column(DateTime(), default=datetime.datetime.now)
+    for_notification  = Column(Boolean(), default=True)
+    notification_sent = Column(Boolean(), default=False)
 
-    def __init__(self, user, action, foreign_data, even_time=datetime.datetime.now(), for_notification=True, notification_sent=True):
-        self.id                        = id
-        self.user                      = user
-        self.action                    = action
-        self.foreign_data              = foreign_data
-        self.event_time                = event_time 
-        self.for_notification          = for_notification
-        self.notification_sent         = notification_sent
+    def __init__(self, user, action, foreign_data, for_notification=None, notification_sent=False, event_time=datetime.datetime.now(), id=get_item_id()):
+        self.id                = id
+        self.user              = user
+        self.action            = action
+        self.foreign_data      = foreign_data
+        self.event_time        = event_time 
+        self.for_notification  = for_notification or consider_for_notification['action']
+        self.notification_sent = notification_sent
 
     def get_id(self):
         return unicode(self.id)
