c9396b5e358a5e16fbdd17c8953c1c53d6b62b3c
Shashank Shekhar <shashankjet@gmail.com>
2014-12-25 14:25:16 +0530

    notifications added

---
+++ franklyapi/notification/__init__.py
@@ -0,0 +1,1 @@
+from config import consider_for_notification
---
+++ franklyapi/notification/config.py
@@ -0,0 +1,9 @@
+
+consider_for_notification = {
+							'follow':True,
+							'like':True,
+							'upvote':True,
+							'answer':True,
+							'question':True,
+							'comment':True
+							}
