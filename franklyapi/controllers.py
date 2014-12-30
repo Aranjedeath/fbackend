3cf9f026fe98969aa7e527c584f84a9d41699299
Shashank Shekhar <shashankjet@gmail.com>
2014-12-31 00:04:09 +0530

    fix question score

--- franklyapi/controllers.py
+++ franklyapi/controllers.py
@@ -1123,7 +1123,7 @@ def discover_posts(cur_user_id, offset, limit, web, lat=None, lon=None):
             count = questions_query.count()
         
         max_limit = count-2 if count>2 else count
-        question_offset = random.random(0, max_limit)
+        question_offset = random.randint(0, max_limit)
         
         questions = questions_query.order_by(Question.score.desc()
                                     ).offset(question_offset
