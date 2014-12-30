0c88cdc70cf3318b9fc72ea44b1c19f7aa945d65
Shashank Shekhar <shashankjet@gmail.com>
2014-12-30 21:53:58 +0530

    redis data database provided

--- franklyapi/periodic_tasks.py
+++ franklyapi/periodic_tasks.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-
+from celery.decorators import periodic_task
 from video_db import update_view_count_to_db, redis_data_client
 
 from configs import config
@@ -7,7 +7,7 @@ from celery import Celery
 
 cel = Celery(broker=config.ASYNC_ENCODER_BROKER_URL, backend=config.ASYNC_ENCODER_BACKEND_URL)
 
-@cel.periodic_task(run_every=timedelta(seconds=10), queue='periodic')
+@periodic_task(run_every=timedelta(seconds=10), queue='periodic')
 def update_view_count():
     print 'started'
     for url in redis_data_client.keys():
