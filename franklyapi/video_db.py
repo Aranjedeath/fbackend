95c0d49eb919a064b77931bb2457972011b00ced
Shashank Shekhar <shashankjet@gmail.com>
2015-01-05 21:55:55 +0530

    rolling back on error

--- franklyapi/video_db.py
+++ franklyapi/video_db.py
@@ -9,58 +9,67 @@ def add_video_to_db(video_url, thumbnail_url=None):
 
 
 def update_video_state(video_url, result={}):
-    if result:
-        print result
-        result.update({'process_state':'success'})
-        Video.query.filter(Video.url==video_url).update(result)
-    else:
-        Video.query.filter(Video.url==video_url).update({'process_state':'failed'})
-        db.session.commit()
+    try:
+        if result:
+            print result
+            result.update({'process_state':'success'})
+            Video.query.filter(Video.url==video_url).update(result)
+        else:
+            Video.query.filter(Video.url==video_url).update({'process_state':'failed'})
+            db.session.commit()
+    except:
+        db.session.rollback()
 
 
 def update_view_count_to_db(url):
-    original_url = url
-    count = redis_data_client.get(url)
-    
-    url = url.replace('http://d35wlof4jnjr70.cloudfront.net/', 'https://s3.amazonaws.com/franklyapp/')
-    types = ['_ultralow', '_low', '_medium', '_opt', '_promo']
-    for suffix in types:
-        url = url.replace(suffix, '')
-    
-    if count:
-        db.session.execute(text("""UPDATE users 
-                                    SET view_count=view_count+:count, total_view_count=total_view_count+:count
-                                    WHERE profile_video=:url
-                                """),
-                            params = {"url":url, "count":int(count)}
-                        )
-        db.session.execute(text("""UPDATE posts 
-                                    SET view_count=view_count+:count 
-                                    WHERE media_url=:url
-                                """),
-                            params = {"url":url, "count":int(count)}
-                        )
-        db.session.commit()
-    redis_data_client.delete(original_url)
-
-def update_total_view_count(user_ids):    
-    result = db.session.execute(text("""SELECT answer_author, sum(posts.view_count)
-                                    AS post_views 
-                                    FROM posts 
-                                    WHERE posts.answer_author in :user_ids"""
-                                    ),
-                                params={'user_ids':user_ids}
-                                )
-    result = dict(list(result))
-    
-    for user_id, post_view_count in result.items():
-        if not post_view_count:
-            post_view_count = 0
+    try:
+        original_url = url
+        count = redis_data_client.get(url)
         
-        db.session.execute(text("""UPDATE users 
-                                    SET total_view_count=view_count+:post_view_count
-                                    WHERE id=:user_id"""
-                            ), params={'user_id':user_id, 'post_view_count':int(post_view_count)})
+        url = url.replace('http://d35wlof4jnjr70.cloudfront.net/', 'https://s3.amazonaws.com/franklyapp/')
+        types = ['_ultralow', '_low', '_medium', '_opt', '_promo']
+        for suffix in types:
+            url = url.replace(suffix, '')
+        
+        if count:
+            db.session.execute(text("""UPDATE users 
+                                        SET view_count=view_count+:count, total_view_count=total_view_count+:count
+                                        WHERE profile_video=:url
+                                    """),
+                                params = {"url":url, "count":int(count)}
+                            )
+            db.session.execute(text("""UPDATE posts 
+                                        SET view_count=view_count+:count 
+                                        WHERE media_url=:url
+                                    """),
+                                params = {"url":url, "count":int(count)}
+                            )
+            db.session.commit()
+        redis_data_client.delete(original_url)
+    except:
+        db.session.rollback()
 
-    db.session.commit()
+def update_total_view_count(user_ids): 
+    try:   
+        result = db.session.execute(text("""SELECT answer_author, sum(posts.view_count)
+                                        AS post_views 
+                                        FROM posts 
+                                        WHERE posts.answer_author in :user_ids"""
+                                        ),
+                                    params={'user_ids':user_ids}
+                                    )
+        result = dict(list(result))
+        
+        for user_id, post_view_count in result.items():
+            if not post_view_count:
+                post_view_count = 0
+            
+            db.session.execute(text("""UPDATE users 
+                                        SET total_view_count=view_count+:post_view_count
+                                        WHERE id=:user_id"""
+                                ), params={'user_id':user_id, 'post_view_count':int(post_view_count)})
+
+        db.session.commit()
+    except:
+        db.session.rollback()
 
