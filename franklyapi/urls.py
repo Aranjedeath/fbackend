3b512fe86d7d7a51bd9b85c825aefccdc9851288
Shashank Shekhar <shashankjet@gmail.com>
2015-01-23 21:20:03 +0530

    printing error

--- franklyapi/urls.py
+++ franklyapi/urls.py
@@ -121,7 +121,7 @@ def apidoc():
         from doc_generator import doc_gen
         from flask import Response
         import resources
-        return Response(json.dumps(doc_gen(app, resources), headers={'Content-Type': 'text/css; charset=utf-8'}))
+        return Response(json.dumps(doc_gen(app, resources)), content_type='application/json')
     except Exception as e:
         import traceback
         print traceback.format_exc(e)
