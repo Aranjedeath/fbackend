d89da813a8a2936373886effcdbfb69197969ba2
Shashank Shekhar <shashankjet@gmail.com>
2015-01-29 15:42:46 +0530

    guni is back

--- runperiodic.sh
+++ runperiodic.sh
@@ -5,7 +5,7 @@ do
 		cd '/home/ubuntu/franklysql/franklyapi'
 		echo "$num restarting periodic celery worker"
 		#alertAdmin 
-		/usr/bin/python /usr/local/bin/celery worker --concurrency=2  -Q encoding --app=periodic_tasks -l info -f /var/periodic_tasks.log --pid /var/periodic_tasks.pid -P gevent &
+		/usr/bin/python /usr/local/bin/celery worker --concurrency=2  -Q encoding --app=periodic_tasks -l info -f /tmp/periodic_tasks.log --pid /tmp/periodic_tasks.pid -P gevent &
 	fi
 	sleep 1
 done
