850c30ecdf2d40a46ca4b848bd4f6d8135048b56
Shashank Shekhar <shashankjet@gmail.com>
2015-01-29 14:40:49 +0530

    loops emoved from bash

--- runperiodic.sh
+++ runperiodic.sh
@@ -1,7 +1,10 @@
-num=`ps -ef|grep python | grep celery|grep cel_tasks|grep encoding|wc -l`
+num=`ps -ef|grep python | grep celery|grep encoding|wc -l`
 if [ $num -lt 1 ]; then
 	cd '/home/ubuntu/franklysql/franklyapi'
 	echo "$num restarting periodic celery worker"
 	#alertAdmin 
 	/usr/bin/python /usr/local/bin/celery worker --concurrency=2  -Q encoding --app=periodic_tasks -l info -f /var/periodic_tasks.log --pid /var/periodic_tasks.pid -P gevent &
 fi
+
+
+* * * * * for i in {0..59}; do cd /home/ubuntu/franklysql; bash runencoder.sh; sleep 1; done;
