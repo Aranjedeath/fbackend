6b08f58a0f6134a979559b8298ffe36709726211
Shashank Shekhar <shashankjet@gmail.com>
2015-01-29 15:35:46 +0530

    guni is back

---
+++ franklyapi/guni_conf.py
@@ -0,0 +1,13 @@
+import multiprocessing
+
+bind = "127.0.0.1:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'gevent'
+worker_connections = 2000
+backlog = 2048
+daemon = True
+loglevel = 'debug'
+accesslog = '/var/guni_access.log'
+errorlog = '/var/guni_error.log'
+max_requests = 1000
+graceful_timeout = 30
