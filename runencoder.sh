7bba5b8ecc77d05b377a71af666b7dc3a9341fb8
Shashank Shekhar <shashankjet@gmail.com>
2015-01-29 15:15:22 +0530

    loops emoved from bash

--- runencoder.sh
+++ runencoder.sh
@@ -1,7 +1,11 @@
-num=`ps -ef|grep python | grep celery|grep encoding|wc -l`
-if [ $num -lt 1 ]; then
-	cd '/home/ubuntu/franklysql/franklyapi'
-	echo "$num restarting encoder worker"
-	#alertAdmin 
-	/usr/bin/python /usr/local/bin/celery worker --concurrency=2  -Q encoding --app=async_encoder -l info -f /var/celery_encoding.log --pid /var/celery_encoding.pid -P gevent &
-fi
+for count in {0..59}
+do
+	num=`ps -ef|grep python | grep celery|grep cel_tasks|grep encoding|wc -l`
+	if [ $num -lt 1 ]; then
+		cd '/home/ubuntu/franklysql/franklyapi'
+		echo "$num restarting encoder worker"
+		#alertAdmin 
+		/usr/bin/python /usr/local/bin/celery worker --concurrency=2  -Q encoding --app=async_encoder -l info -f /var/celery_encoding.log --pid /var/celery_encoding.pid -P gevent &
+	fi
+	sleep 1
+done
--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,11 @@
-num=`ps -ef|grep python|grep gunicorn|grep app|wc -l`
-if [ $num -lt 1 ]; then
-	cd '/home/ubuntu/franklysql/'
-	echo "$num restarting urgent worker"
-	#alertAdmin 
-	/bin/bash guni
-fi
+for count in {0..59}
+do
+	num=`ps -ef|grep python|grep gunicorn|grep app|wc -l`
+	if [ $num -lt 1 ]; then
+		cd '/home/ubuntu/franklysql/'
+		echo "$num restarting urgent worker"
+		#alertAdmin 
+		/bin/bash guni
+	fi
+	sleep 1
+done
