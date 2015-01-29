a87565c8b6bfe13a3c2905e33c9ed35dfe420452
Shashank Shekhar <shashankjet@gmail.com>
2015-01-29 14:31:37 +0530

    loops emoved from bash

--- runserver.sh
+++ runserver.sh
@@ -1,11 +1,7 @@
-for count in 1 2 3 4 5 6 7 8 9 10 11 12
-do
-	num=`ps -ef|grep python|grep gunicorn|grep app|wc -l`
-	if [ $num -lt 1 ]; then
-		cd '/home/ubuntu/franklysql/'
-		echo "$num restarting urgent worker"
-		#alertAdmin 
-		/bin/bash guni
-	fi
-	sleep 5
-done
+num=`ps -ef|grep python|grep gunicorn|grep app|wc -l`
+if [ $num -lt 1 ]; then
+	cd '/home/ubuntu/franklysql/'
+	echo "$num restarting urgent worker"
+	#alertAdmin 
+	/bin/bash guni
+fi
