6efdf265ba87d1c1f0759c511dc54f7a5e347165
Shashank Shekhar <shashankjet@gmail.com>
2015-02-02 19:15:06 +0530

    version code fixed

--- franklyapi/controllers.py
+++ franklyapi/controllers.py
@@ -947,7 +947,7 @@ def question_list_public(current_user_id, user_id, offset, limit, version_code=N
 
     cur_user_questions = []
 
-    if offset == 0 and current_user_id and version_code>43:
+    if offset == 0 and current_user_id and version_code>47:
         cur_user_questions = Question.query.filter(Question.question_to==user_id,
                                                     Question.question_author==current_user_id,
                                                     Question.deleted==False,
