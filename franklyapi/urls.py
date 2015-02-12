9415d9a44af6ddbef20b1245cad0d33a134d42db
Shashank Shekhar <shashankjet@gmail.com>
2015-02-12 19:08:49 +0530

    channel_list_implememnted

--- franklyapi/urls.py
+++ franklyapi/urls.py
@@ -88,6 +88,8 @@ api.add_resource(InviteCeleb, '/invite/celeb')
 api.add_resource(FeedBackResponse, '/feedback')
 
 api.add_resource(ChannelFeed, '/channel/<channel_id>')
+api.add_resource(ChannelList, '/channel/list')
+
 
 
 
