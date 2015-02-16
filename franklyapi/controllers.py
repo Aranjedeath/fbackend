7a3f071bdeb608c90185c7fc2c70628740340533
Rishabh Goel <goelrishabh09@gmail.com>
2015-02-16 18:45:06 +0530

    timeline user offset bug fixed

--- franklyapi/controllers.py
+++ franklyapi/controllers.py
@@ -1322,12 +1322,10 @@ def comment_list(cur_user_id, post_id, offset, limit):
 
 
 def get_user_timeline(cur_user_id, user_id, offset, limit, include_reshares=False):
+    
     if cur_user_id and has_blocked(cur_user_id, user_id):
         raise CustomExceptions.UserNotFoundException('User does not exist')
-
-
-
-
+    
     if include_reshares:    
         posts_query = Post.query.filter(or_(Post.answer_author==user_id,
                                             Post.id.in_(Reshare.query.filter(Reshare.user==user_id))
@@ -1340,10 +1338,13 @@ def get_user_timeline(cur_user_id, user_id, offset, limit, include_reshares=Fals
         posts_query = Post.query.filter(Post.answer_author==user_id, Post.deleted==False).order_by(Post.timestamp.desc())
 
     total_count = posts_query.count()
+    if offset == -1:
+        return {'stream': [], 'count':0, 'next_index':-1, 'total':total_count}
     posts = posts_query.offset(offset).limit(limit).all()
     posts = posts_to_dict(posts, cur_user_id)
     posts = [{'type':'post', 'post':post} for post in posts]
     next_index = offset+limit if posts else -1
+    
     return {'stream': posts, 'count':len(posts), 'next_index':next_index, 'total':total_count}
     
 
