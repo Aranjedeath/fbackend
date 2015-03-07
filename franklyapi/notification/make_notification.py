fbbb787c83070a128d8bccd79bfb049a0fd4161d
Shashank Shekhar <shashankjet@gmail.com>
2015-03-08 04:47:01 +0530

    testing notifications

--- franklyapi/notification/make_notification.py
+++ franklyapi/notification/make_notification.py
@@ -67,7 +67,7 @@ def push_notification(notification_id, user_id, source='application'):
                     "id" : user_push_notification.id,
                     "text" : notification.text,
                     "icon" : None,
-                    "group_id": group_id,
+                    "group_id": 3,#group_id,
                     "link" : notification.link,
                     "deeplink" : notification.link,
                     "timestamp" : int(time.mktime(user_push_notification.added_at.timetuple())),
