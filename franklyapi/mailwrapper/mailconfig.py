35f5ad958ea141904ad3071376dd1ee71dc2361f
hellking4u <hellking4u@gmail.com>
2015-03-13 20:37:29 +0530

    A barebone mailer is done

--- franklyapi/mailwrapper/mailconfig.py
+++ franklyapi/mailwrapper/mailconfig.py
@@ -1,2 +1,7 @@
-aws_access_key_id='AKIAJQQLEO5Q4TXXD2ZQ'
-aws_secret_access_key='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'
+from configs import config
+
+aws_access_key_id = config.AWS_KEY
+aws_secret_access_key = config.AWS_SECRET
+
+#aws_access_key_id='AKIAJQQLEO5Q4TXXD2ZQ'
+#aws_secret_access_key='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'
--- franklyapi/mailwrapper/temp.py
+++ franklyapi/mailwrapper/temp.py
@@ -5,10 +5,6 @@ from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
-conn = boto.ses.connect_to_region(
-    'us-east-1',
-    aws_access_key_id='AKIAJQQLEO5Q4TXXD2ZQ',
-    aws_secret_access_key='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')
 
 
 def add_email_sender(mail_id):
@@ -18,16 +14,6 @@ def add_email_sender(mail_id):
 def get_verified_emails():
     return conn.list_verified_email_addresses()['ListVerifiedEmailAddressesResponse']['ListVerifiedEmailAddressesResult']['VerifiedEmailAddresses']
 # print add_email_sender('nikhil@frankly.me')
-
-
-def send_mail(sender_id, reciever_id, message_subject, message_body):
-    conn.send_email(
-        sender_id,
-        message_subject,
-        reciever_id,
-        [reciever_id],
-        format='html',
-        bcc_addresses='nikhil@frankly.me')
 
 
 @app.route('/endpoint', methods=['GET', 'POST'])
