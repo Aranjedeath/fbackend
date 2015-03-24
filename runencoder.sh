aa4986324db885577603b6eab3adab2c6e166cf2
Media_encoder_server <you@example.com>
2015-03-24 10:46:43 +0000

    video now working

--- runencoder.sh
+++ runencoder.sh
@@ -5,7 +5,9 @@ do
 		cd '/home/ubuntu/franklysql/franklyapi'
 		echo "$num restarting encoder worker"
 		#alertAdmin 
-		/usr/bin/python /usr/local/bin/celery worker --concurrency=2  -Q encoding --app=async_encoder -l info -f /tmp/celery_encoding.log --pid /tmp/celery_encoding.pid -P gevent &
+		/usr/bin/python /usr/local/bin/celery worker --concurrency=3 --autoreload --maxtasksperchild=10 -Ofair  -Q encoding --app=async_encoder -l info -f /tmp/celery_encoding.log --pid /tmp/celery_encoding.pid -P gevent&
 	fi
 	sleep 1
 done
+
+
