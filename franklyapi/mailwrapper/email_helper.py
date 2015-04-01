4c794cdb8cff766b3e0d76177f889d05270a5b83
Rishabh Goel <goelrishabh09@gmail.com>
2015-04-01 19:29:27 +0530

    minor code clean : email_helper.py

--- franklyapi/mailwrapper/email_helper.py
+++ franklyapi/mailwrapper/email_helper.py
@@ -7,7 +7,7 @@ import os
 
 
 
-env = Environment(loader = PackageLoader('mailwrapper','mail_templates'))
+env = Environment(loader=PackageLoader('mailwrapper', 'mail_templates'))
 
 header_template = env.get_template('standard_email.html')
 
@@ -24,7 +24,8 @@ render_dict = {
 mail_sender = SimpleMailer('Frankly@franklymail.com')
 
 
-def welcome_mail(receiver_email,receiver_name,receiver_username,receiver_password):
+def welcome_mail(receiver_email, receiver_name,
+    receiver_username, receiver_password):
 
     render_dict['salutation'] = "Hi %s" % receiver_name
     render_dict['email_text'] = mail_content.dict['welcome_mail']['body'] % (receiver_username, receiver_password)
@@ -34,14 +35,18 @@ def welcome_mail(receiver_email,receiver_name,receiver_username,receiver_passwor
 
 
 def forgot_password(receiver_email, token, reciever_name):
-    url = os.path.join(config.WEB_URL, 'reset-password?token={token}'.format(token=token))
+    url = os.path.join(config.WEB_URL,
+            'reset-password?token={token}'.format(token=token))
 
 
-    render_dict['salutation'] = "Hi {reciever_name}".format(reciever_name=str(reciever_name))
-    render_dict['email_text'] = mail_content.dict['forgot_password']['body'].format(reset_password_link=url)
+    render_dict['salutation'] = "Hi {reciever_name}".format(
+            reciever_name=str(reciever_name))
+    render_dict['email_text'] = mail_content.dict['forgot_password']['body'].format(
+            reset_password_link=url)
 
 
-    mail_sender.send_mail(receiver_email, mail_content.dict['forgot_password']['subject'],
+    mail_sender.send_mail(receiver_email,
+                          mail_content.dict['forgot_password']['subject'],
                           header_template.render(render_dict))
 
 
@@ -52,7 +57,8 @@ def question_asked(receiver_email, receiver_name, question_to_name, is_first):
     if is_first:
         render_dict['email_text'] = mail_content.dict['question_asked']['body_first_question']
 
-    mail_sender.send_mail(receiver_email,mail_content.dict['question_asked']['subject'],
+    mail_sender.send_mail(receiver_email,
+                          mail_content.dict['question_asked']['subject'],
                           header_template.render(render_dict))
 
 
@@ -61,19 +67,21 @@ def question_answered(receiver_email, receiver_name, celebrity_name, question, w
     render_dict['salutation'] = "Hi %s" % receiver_name
     render_dict['email_text'] = mail_content.dict['question_answered']['body'] % (celebrity_name, web_link, question)
 
-    mail_sender.send_mail(receiver_email, mail_content.dict['question_answered']['subject'],
+    mail_sender.send_mail(receiver_email,
+                          mail_content.dict['question_answered']['subject'],
                           header_template.render(render_dict))
 
 #Weekly Email
-def inactive_profile(receiver_email,receiver_name):
+def inactive_profile(receiver_email, receiver_name):
     render_dict['salutation'] = "Hi %s" % receiver_name
     render_dict['email_text'] = mail_content.dict['inactive_profile']['body']
 
-    mail_sender.send_mail(receiver_email ,mail_content.dict['inactive_profile']['subject'],
+    mail_sender.send_mail(receiver_email,
+                          mail_content.dict['inactive_profile']['subject'],
                           header_template.render(render_dict))
 
 
-def send_weekly_report(recipients,report):
+def send_weekly_report(recipients, report):
     mail_sender.send_mail(recipients, "Weekly Report", report)
 
 
@@ -85,9 +93,10 @@ def cron_job_update(cron_type="Dhak Dhak", message='Keep running!'):
     mail_sender.send_mail(config.DEV_EMAILS, cron_type, message)
 
 
-def send_mail_for_sapru(receiver_email,receiver_name,link):
+def send_mail_for_sapru(receiver_email, receiver_name, link):
     render_dict['salutation'] = "Hi %s" % receiver_name
     render_dict['email_text'] = mail_content.dict['sapru']['body'] % link
     render_dict['signature']  = personal_signature
-    mail_sender.send_mail(receiver_email,mail_content.dict['sapru']['subject'],
+    mail_sender.send_mail(receiver_email,
+                          mail_content.dict['sapru']['subject'],
                           header_template.render(render_dict))
