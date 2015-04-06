7d917f7e46ac8dbdf80a0261efcf0365365d0f11
Varun <quizots@gmail.com>
2015-04-06 18:40:26 +0530

    Separated admin email from normal mail

---
+++ franklyapi/mail/admin_email.py
@@ -0,0 +1,22 @@
+from configs import config
+from infra import SimpleMailer
+
+mail_sender = SimpleMailer(config.FROM_EMAIL_ADDRESS)
+
+
+# ADMIN EMAILS
+def send_weekly_report(recipients, report):
+    mail_sender.send_mail(recipients, "Weekly Report", report)
+
+
+def content_report(recipients, subject, report):
+    mail_sender.send_mail(recipients, subject, report)
+
+
+def cron_job_update(cron_type="Cron heartbeat", message='You only die once - YODO'):
+    mail_sender.send_mail(config.DEV_EMAILS, cron_type, message)
+
+
+def push_stats(body):
+    mail_sender.send_mail(['varun@frankly.me','abhishek@frankly.me','nikunj@frankly.me'],
+                           'Push Notification Stats', body)
