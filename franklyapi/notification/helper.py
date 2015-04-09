7b76bc84ab66d05ffe4b9120f75773364d7e4207
Varun <quizots@gmail.com>
2015-04-09 16:41:32 +0530

    Minor bug fix

--- franklyapi/notification/helper.py
+++ franklyapi/notification/helper.py
@@ -69,7 +69,7 @@ key = {
     'intro-video-request':{
          'title': 'You are in demand!',
          'text': '<requester_name> just asked you for an intro video.',
-         'url': config.WEB_URL + '/u/   %s',
+         'url': config.WEB_URL + '/u/%s',
          'day-limit': 1,
          'label_one':'',
          'label_two': ''
