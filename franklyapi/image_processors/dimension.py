74b55ebb9a8244bb6c6c2c8e93588a5488a6925e
amritanshu dwivedi <amritanshu.dwivedi@inoxapps.com>
2015-04-15 17:18:18 +0530

    incomplete

--- franklyapi/image_processors/dimension.py
+++ franklyapi/image_processors/dimension.py
@@ -5,21 +5,25 @@ def toDimensions(image_file, out_file, dim_x, dim_y, allowLow=True, return_file_
     im = Image.open(image_file)
     im_x = im.size[0]
     im_y = im.size[1]
+    print 'im_x: %s' %im_x
+    print 'im_y: %s' %im_y 
     flag = True
     if not allowLow:
         flag = (im_x >= dim_x) and (im_y >= dim_y)
+    print 'flag: %s' %flag
     if flag:
-        aspectRatioIn = im_x / float(im_y)
-        aspectRatioOut = dim_x / float(dim_y)
-        if(aspectRatioIn > aspectRatioOut):
-            fin_y = im_y
-            fin_x = im_y * aspectRatioOut
-        else:
-            fin_x = im_x
-            fin_y = im_x * aspectRatioOut
-        box = (int((im_x - fin_x)/2), int((im_y - fin_y)/2), int((im_x + fin_x)/2), int((im_y + fin_y)/2) )
+        box = get_box(im_x=im_x, im_y=im_y, dim_x=dim_x, dim_y=dim_y)
+        print 'box: ' + str(box)
         im = im.crop(box)
+        im_x = im.size[0]
+        im_y = im.size[1]
+        print 'im_x: %s' %im_x
+        print 'im_y: %s' %im_y 
         im = im.resize((dim_x,dim_y))
+        im_x = im.size[0]
+        im_y = im.size[1]
+        print 'im_x: ' + im_x
+        print 'im_y: ' + im_y 
         
         if return_file_object:
             from StringIO import StringIO
@@ -31,6 +35,37 @@ def toDimensions(image_file, out_file, dim_x, dim_y, allowLow=True, return_file_
         return out_file
     else:
         raise Exception("Small Size")
+
+def get_box(im_x, im_y, dim_x, dim_y):
+    aspectRatioIn = im_x / float(im_y)
+    aspectRatioOut = dim_x / float(dim_y)
+    print 'ari: %s' %aspectRatioIn
+    print 'aro: %s' %aspectRatioOut
+    if(aspectRatioIn > aspectRatioOut):
+        if(aspectRatioOut < 1):
+            if(aspectRatioIn > 1):
+                fin_x = im_x
+                fin_y = im_x * aspectRatioOut
+            else:
+                fin_y = im_x/aspectRatioOut
+                fin_x = im_x
+        else:
+            fin_y = im_y
+            fin_x = im_y * aspectRatioOut
+    else:
+        if(aspectRatioOut < 1):
+            if(aspectRatioIn > 1):
+                fin_x = im_x
+                fin_y = im_x * aspectRatioOut
+            else:
+                fin_y = im_x/aspectRatioOut
+                fin_x = im_x
+        else:
+            fin_x = im_x
+            fin_y = im_x * aspectRatioOut
+    print 'fin_x: %s\nfin_y: %s' %(fin_x, fin_y)
+    box = (int((im_x - fin_x)/2), int((im_y - fin_y)/2), int((im_x + fin_x)/2), int((im_y + fin_y)/2) )
+    return box
 
 
 def sanitize_profile_pic(image_file):
